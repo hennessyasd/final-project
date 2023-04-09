1253826946959a3f4f99f935b7c9669172e3cb98
hennessyasd <102168763+hennessyasd@users.noreply.github.com>
2023-04-09 22:41:37 +0300

    Changes#19
    Complete a few things.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-// let bLearnMore = document.getElementById(bLearnMore);
-
-// bLearnMore.addEventListener('click');
+//element.removeEventListener(event, handler, [options]);
+let x = document.getElementsByClassName('b2');
+x.onclick = function() {
+    window.location.href = 'portfolioPage.html';
+};
