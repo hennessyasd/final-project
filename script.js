a35e5c686a0ab47bce057a038324805f3511937d
hennessyasd <102168763+hennessyasd@users.noreply.github.com>
2023-04-11 18:26:28 +0300

    Changes#21
    Complete full site

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
 //element.removeEventListener(event, handler, [options]);
-let x = document.getElementsByClassName('b2');
-x.onclick = function() {
-    window.location.href = 'portfolioPage.html';
-};
+
